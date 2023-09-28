9df33490889439e12c301beaaf1479582528cc65
Andrelegrand <andremabutwa1@gmail.com>
2023-09-28 09:56:49 +0200

    update index

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const message = ['Bonjour chers amis, nous sommes le groupe N1','Nous bossons a la Kadea','Nous travaillons sur le projet mixed message'];
+function generatemessage() {
+    const messages = Math.floor(Math.random()*message.length);
+    const messagemexed = message[messages];
+    return messagemexed;
+};
 
+   
